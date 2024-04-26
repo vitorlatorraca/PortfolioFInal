e1d1220d74ea284184c09865af332d26291cfeeb
Vitor Latorraca <vitorlatorraca5@gmail.com>
2024-04-26 01:23:20 -0400

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import About from './components/about/About';
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import Services from "./components/services/Services";
+import Skills from "./components/skills/Skills";
+import Work from "./components/work/Work";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Header />
+    <main className="main">
+      <Home/>
+      <About/>
+      <Skills/>
+      <Work/>
+      <Services />
+      <Contact />
+      <Footer />
+    </main>
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+        <div className="footer__container container">
+            <h1 className="footer__title">Latorraca</h1>
+
+            <ul className="footer__list">
+                <li>
+                    <a href="#about" className="footer__link">About</a>
+                </li>
+
+                <li>
+                    <a href="#portfolio" className="footer__link">Projects</a>
+                </li>
+
+                <li>
+                    <a href="skills" className="footer__link">Skills</a>
+                </li>
+            </ul>
+                <div className="footer__social">
+                <a href="https://www.instagram.com/vitorlatorraca"
+        className="footer__social-link" target="_blank">
+            <i class="bx bxl-instagram"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/vitor-latorraca-47430620a"
+        className="footer__social-link" target="_blank">
+        <i class="uil uil-dribbble"></i>
+        </a>
+
+        <a href="https://github.com/vitorlatorraca"
+        className="footer__social-link" target="_blank">
+        <i class="uil uil-github-alt "></i>
+        </a>
+        </div>
+        <span className="footer__copy"> All rigths reserved</span>
+        </div>
+       
+    </footer>
+  )
+}
+
+export default Footer
