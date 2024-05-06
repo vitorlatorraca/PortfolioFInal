f0ce4b86ac25c96e9388ba52d621971b996800ee
Vitor Latorraca <vitorlatorraca5@gmail.com>
2024-05-06 00:41:51 -0400

    new

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,8 +1,4 @@
 import Work1 from "../../assets/work1.jpg"
-import Work2 from "../../assets/work2.jpg"
-import Work3 from "../../assets/work3.jpg"
-import Work4 from "../../assets/work4.jpg"
-import Work5 from "../../assets/work5.jpg"
 
 
 export const projectsData = [
@@ -13,7 +9,7 @@ export const projectsData = [
         category: 'web',
     },
 
-    {
+   /* {
         id: 2,
         image: Work2,
         title: 'Web design',
@@ -39,7 +35,7 @@ export const projectsData = [
         image: Work5,
         title: 'Web design',
         category: 'Web',
-    },
+    },*/
 ];
 
 export const projectsNav = [
