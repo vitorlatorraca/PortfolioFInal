a1bd302132ffe41b545ce771761297a3cb675f6f
Vitor Latorraca <vitorlatorraca5@gmail.com>
2024-06-19 22:48:54 -0400

    new

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -2,7 +2,7 @@ import Work1 from "../../assets/work1.jpg";
 import Work2 from "../../assets/work2.jpg";
 import Work3 from "../../assets/work3.png";
 import Work4 from "../../assets/work4.jpg";
-import
+import Work5 from "../../assets/work5.jpg"
 
 export const projectsData = [
     {
