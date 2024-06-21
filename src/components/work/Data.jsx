5d681d4c77b903b710f9370a5045b242e050989a
Vitor Latorraca <vitorlatorraca5@gmail.com>
2024-06-20 23:44:31 -0400

    new

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,47 +1,43 @@
-import Work1 from "../../assets/work1.jpg";
-import Work2 from "../../assets/work2.jpg";
-import Work3 from "../../assets/work3.png";
-import Work4 from "../../assets/work4.jpg";
-import Work5 from "../../assets/work5.jpg"
+import Work1 from "../../assets/image1.png";
+import Work2 from "../../assets/image2.jpg";
+import Work3 from "../../assets/image3.png";
+import Work4 from "../../assets/image4.jpg";
+import Work5 from "../../assets/georgian.jpg";
 
 export const projectsData = [
     {
         id: 1,
         image: Work1,
-        title: 'Coffee Delivery Website',
+        title: 'FutGram',
+        category: 'App',
+        demoUrl: 'https://github.com/vitorlatorraca/futGram'
+    },
+    {
+        id: 2,
+        image: Work3,
+        title: 'Coffe Delivery',
         category: 'web',
         demoUrl: 'https://github.com/vitorlatorraca/coffeDelivery'
     },
-
     {
-        id: 2,
-        image: Work2,
-        title: 'Tic Tac Toe Game',
-        category: 'web',
-        demoUrl: 'https://github.com/vitorlatorraca/TicTacToe'
-    },
-
-   {
         id: 3,
-        image: Work3,
-        title: 'Major Assingment CSS',
-        category: 'design',
-        demoUrl: 'https://github.com/vitorlatorraca/Major_Assingment_CSS'
+        image: Work5,
+        title: 'Time Tracker',
+        category: 'College',
+        demoUrl: 'https://github.com/vitorlatorraca/TImeTracker'
     },
-
-     {
+    {
         id: 4,
-        image: Work4,
-        title: 'Web design',
-        category: 'design',
-        demoUrl: 'https://github.com/vitorlatorraca/Major_Assingment_CSS'
+        image: Work5,
+        title: 'Soccer Quiz',
+        category: 'College',
+        demoUrl: 'https://globo.com'
     },
-
     {
         id: 5,
         image: Work5,
-        title: 'Web design',
-        category: 'Web',
+        title: 'File Manager JAVA',
+        category: 'College',
         demoUrl: 'https://github.com/vitorlatorraca/Major_Assingment_CSS'
     },
 ];
@@ -60,4 +56,3 @@ export const projectsNav = [
         name: 'design',
     },
 ];
- 
