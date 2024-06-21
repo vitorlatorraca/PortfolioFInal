b253b1af83c879e16df6a4046846c11b986054f3
Vitor Latorraca <vitorlatorraca5@gmail.com>
2024-06-20 23:50:11 -0400

    new

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Work1 from "../../assets/image1.png";
 import Work2 from "../../assets/image2.jpg";
 import Work3 from "../../assets/image3.png";
@@ -10,35 +11,40 @@ export const projectsData = [
         image: Work1,
         title: 'FutGram',
         category: 'App',
-        demoUrl: 'https://github.com/vitorlatorraca/futGram'
+        demoUrl: 'https://github.com/vitorlatorraca/futGram',
+        bio: 'A social network with a database that enables users to create accounts, make posts, like other posts, and more utilities.'
     },
     {
         id: 2,
         image: Work3,
-        title: 'Coffe Delivery',
+        title: 'Coffee Delivery',
         category: 'web',
-        demoUrl: 'https://github.com/vitorlatorraca/coffeDelivery'
+        demoUrl: 'https://github.com/vitorlatorraca/coffeDelivery',
+        bio: 'An e-commerce platform for coffee, prioritizing Brazilian customers.'
     },
     {
         id: 3,
         image: Work5,
         title: 'Time Tracker',
         category: 'College',
-        demoUrl: 'https://github.com/vitorlatorraca/TImeTracker'
+        demoUrl: 'https://github.com/vitorlatorraca/TImeTracker',
+        bio: 'A final group project for the Programming Fundamentals course. It simulates a time tracker.'
     },
     {
         id: 4,
         image: Work5,
         title: 'Soccer Quiz',
         category: 'College',
-        demoUrl: 'https://globo.com'
+        demoUrl: 'https://globo.com',
+        bio: 'A football quiz.'
     },
     {
         id: 5,
         image: Work5,
         title: 'File Manager JAVA',
         category: 'College',
-        demoUrl: 'https://github.com/vitorlatorraca/Major_Assingment_CSS'
+        demoUrl: 'https://github.com/vitorlatorraca/Major_Assingment_CSS',
+        bio: 'A Java assignment that opened my eyes to some programming fundamentals.'
     },
 ];
 
@@ -56,3 +62,23 @@ export const projectsNav = [
         name: 'design',
     },
 ];
+
+const Project = ({ project }) => (
+    <div>
+        <img src={project.image} alt={project.title} />
+        <h2>{project.title}</h2>
+        <p>{project.category}</p>
+        <a href={project.demoUrl}>Demo</a>
+        <p>{project.bio}</p>
+    </div>
+);
+
+const Projects = () => (
+    <div>
+        {projectsData.map(project => (
+            <Project key={project.id} project={project} />
+        ))}
+    </div>
+);
+
+export default Projects;
